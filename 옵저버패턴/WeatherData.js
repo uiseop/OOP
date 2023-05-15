2d4427312ad44838b2ba7762ee76dca2e4378047
uiseop <bono521@naver.com>
2023-05-15 16:32:36 +0900

    Add: WeatherData가 가져다주는 정보, 행동, 역할

---
+++ 옵저버패턴/WeatherData.js
@@ -0,0 +1,23 @@
+class WeatherData {
+  constructor() {
+    this.humidity = null;
+    this.temperature = null;
+    this.pressure = null;
+  }
+
+  getHumidity() {
+    // 습도를 가져오기
+  }
+
+  getTemperature() {
+    // 온도 가져오기
+  }
+
+  getPressure() {
+    // 기압 가져오기
+  }
+
+  update() {
+    // 구독자들에게 변화를 알리기
+  }
+}
